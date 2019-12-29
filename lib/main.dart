44a855d33abbefb8e66b0aefdfdebf6f72a3ab51
edilson14 <edytavares66@gmail.com>
2019-12-29 15:22:02 -0300

    FLUTTER Diagrama de Widgets ScallFold - oferece a estrutura para o App AppBar - title, color , centerTitle, body - onde fica o conteúdo principal do app Image - Widgets para usar imagens ( existe da dart:ui e das Widgets )       - Imagens podem ser inseridas de varias formas NetworkImage - aceita uma url da imagem na internet para mostar ( formato string )
    ANDROID STUDIO
        Usar atalhos para inserir os Widgets entre um Widget pai , no caso usei o Center
        para centralizar Imagens

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,21 @@ import 'package:flutter/material.dart';
 void main() {
   // Respnsavél pela execução do programa
   runApp(
+    // Incluindo materialApp temos acesso aos widgets, cores
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://1.bp.blogspot.com/-ak_aEW48mmA/XNCDH1UYfaI/AAAAAAAAHeg/NUhgwcoZX2Ei7exfQreLRSmOfPtxdCReQCLcBGAs/s1600/image2.png'),
+          ),
+        ),
       ),
     ),
   );
