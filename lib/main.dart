1187125a561591c6dab0027adabe7364bfd80dc6
edilson14 <edytavares66@gmail.com>
2020-01-01 19:01:19 -0300

    (assets , pubspec.yaml)     forma de trabalhar com os assets inlcuir imagens e utilização de pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 
-// Ponto de Entrada do app toda vez que o app se inicializar verifica o qu fazer aqui
-void main() {
-  // Respnsavél pela execução do programa
-  runApp(
-    // Incluindo materialApp temos acesso aos widgets, cores
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('I Am Rich'),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://1.bp.blogspot.com/-ak_aEW48mmA/XNCDH1UYfaI/AAAAAAAAHeg/NUhgwcoZX2Ei7exfQreLRSmOfPtxdCReQCLcBGAs/s1600/image2.png'),
+// Ponto de Entrada do app toda vez que o app se inicializar verifica o que fazer aqui
+void main() =>
+    // Respnsavél pela execução do programa
+    runApp(
+      // Incluindo materialApp temos acesso aos widgets, cores
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(image: AssetImage('images/diamond.png')),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
